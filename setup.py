c5b27cc87a30c565666c977732d17c2cf5842ae5
Oliver <joshua.oliver@wsu.edu>
2025-08-18 12:01:15 -0700

    Bumped version number for testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crop_model",
-    version="0.1.0",
+    version="0.1.1",
     author="liuming",
     description="Simulation engine for AgWeatherNet Nutrient Management System",
     packages=find_packages(),
