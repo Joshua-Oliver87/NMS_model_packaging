ac0a42da2367d3d5f7aae8492359787e968bdb6c
Oliver <joshua.oliver@wsu.edu>
2025-08-18 12:54:40 -0700

    Bump version to 0.1.2 for workflow testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crop_model",
-    version="0.1.1",
+    version="0.1.2",
     author="liuming",
     description="Simulation engine for AgWeatherNet Nutrient Management System",
     packages=find_packages(),
